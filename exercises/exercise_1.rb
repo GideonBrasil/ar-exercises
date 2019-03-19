020bb94a43701c667c7d2170800f85cd4e86991e
GideonBrasil <gidxx_z@yahoo.com>
2019-03-19 20:05:28 +0000

    fix up code style

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -1,10 +1,15 @@
 require_relative '../setup'
 
-puts "Exercise 1"
-puts "----------"
+puts 'Exercise 1'
+puts '----------'
 
-stores = Store.create(name: 'Burnaby', annual_revenue: 300000, mens_apparel: true, womens_apparel: true )
-stores = Store.create(name: 'Richmond', annual_revenue: 1260000, mens_apparel: false, womens_apparel: true )
-stores = Store.create(name: 'Gastown', annual_revenue: 190000, mens_apparel: true, womens_apparel: false )
+stores = Store.create(name: 'Burnaby', annual_revenue: 300_000,
+                      mens_apparel: true, womens_apparel: true)
+stores = Store.create(name: 'Richmond', annual_revenue: 1_260_000,
+                      mens_apparel: false, womens_apparel: true)
+stores = Store.create(name: 'Gastown', annual_revenue: 190_000,
+                      mens_apparel: true, womens_apparel: false)
 
 puts Store.count
+
+@store1 = Store.find_by(id: 1)
